cad0391904abeaac0b89edeaa4c484a7ef6a34ca
Franco Garcia Carril <garciacfranco2001@gmail.com>
2024-03-02 17:08:11 -0300

    iniciando nuevamente

--- esthetic_school/esthetic_school/views/pruebas.py
+++ esthetic_school/esthetic_school/views/pruebas.py
@@ -1,13 +1,14 @@
 import reflex as rx
 
 from esthetic_school.views.wellcome import wellcome 
-from esthetic_school.views.login import login
-from esthetic_school.views.courses import courses
+from esthetic_school.views.course import course
 from esthetic_school.views.register import register
+from esthetic_school.views.payment import payment 
+from esthetic_school.states.states import show_information
 
 def pruebas() -> rx.Component:
     return rx.center(
-        login(),
+        show_information(),
         width="100%",
         height="100vh",
         align="center",
